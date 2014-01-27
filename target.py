65b5a1e699716989372e74c3bfdba47ca261255c
Wasiur Rahman <wrahman0@gmail.com>
2014-01-26 20:48:42 -0500

    Fixing location detection bug + Changes the data structure

--- target.py
+++ target.py
@@ -1,45 +1,54 @@
 #!/usr/bin/env python
 import scraperwiki           
 import lxml.html           
+
+
 html = scraperwiki.scrape("https://listings.och.uwaterloo.ca/Listings/Search/Results")
 root = lxml.html.fromstring(html)
-master = []
+
+addressCheck1 = "Waterloo, ON"
+addressCheck2 = "waterloo, ON"
+addressCheck3 = "Cambridge, ON"
+addressCheck4 = "cambridge, ON"
+addressCheck5 = "Kitchener, ON"
+addressCheck6 = "kitchener, ON"
+
+address = "https://listings.och.uwaterloo.ca/Listings/Search/Results?page="
+master = {}
 nameList = []
 priceList = []
-addressCheck = "Waterloo, ON"
-address = "https://listings.och.uwaterloo.ca/Listings/Search/Results?page="
 page = 0
+numpages = 26
 
-while (page!=7):
+while (page!=numpages):
 	#Redirecting to new 
 	html = scraperwiki.scrape(address+str(page+1))
 	root = lxml.html.fromstring(html)
 
 	#Targeting the names and location
+	#Name of the location
 	for tr in root.cssselect("div#Rentals tr td span a"):
+	    print "Scraping Names Page:" + str(page) + "..."
 	    name = str.strip(tr.text_content())
-	    if addressCheck in name:
-	        print name
+	    if addressCheck1 in name or addressCheck2 in name or addressCheck3 in name or addressCheck4 in name or addressCheck5 in name or addressCheck6 in name:
+	       # print name
 	        nameList.append(name)
 	    
 	#Price Listing
 	for tr in root.cssselect(".t-last"):
+	    print "Scraping Prices Page:" + str(page) + "..."
 	    price = str.strip(tr.text_content())
 	    price = ''.join(price.split())
-	    # print price
+	   # print price
 	    priceList.append(price)
 
 	page+=1
-    
-    
+
 i = 0
-while(i!=len(nameList) and i!=len(priceList)):
-    temp = {"Name" : nameList[i],
-              "Price": priceList[i]
-             }
-    print temp
-    master.append(temp)
+while (i!=len(nameList) and i!=len(priceList)):
+    master[nameList[i]] = priceList[i]
     i+=1
-
     
 
+for location,price in master.items():
+	print "Name: " + location +"  ---  " + "Price: " + price
