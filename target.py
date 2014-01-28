78cf476a2a6a09c65e89e8de3a3c6dea06d94d16
Wasiur Rahman <wrahman0@gmail.com>
2014-01-27 19:38:40 -0500

    Implement sort by price

--- target.py
+++ target.py
@@ -2,7 +2,6 @@
 import scraperwiki           
 import lxml.html           
 
-
 html = scraperwiki.scrape("https://listings.och.uwaterloo.ca/Listings/Search/Results")
 root = lxml.html.fromstring(html)
 
@@ -13,42 +12,63 @@ addressCheck4 = "cambridge, ON"
 addressCheck5 = "Kitchener, ON"
 addressCheck6 = "kitchener, ON"
 
-address = "https://listings.och.uwaterloo.ca/Listings/Search/Results?page="
-master = {}
-nameList = []
-priceList = []
-page = 0
-numpages = 26
+def scrape():
+	address = "https://listings.och.uwaterloo.ca/Listings/Search/Results?page="
+	master = {}
+	nameList = []
+	priceList = []
+	page = 0
+	numpages = 1
 
-while (page!=numpages):
-	#Redirecting to new 
-	html = scraperwiki.scrape(address+str(page+1))
-	root = lxml.html.fromstring(html)
+	while (page!=numpages):
+		#Redirecting to new 
+		html = scraperwiki.scrape(address+str(page+1))
+		root = lxml.html.fromstring(html)
 
-	#Targeting the names and location
-	#Name of the location
-	for tr in root.cssselect("div#Rentals tr td span a"):
-	    print "Scraping Names Page:" + str(page) + "..."
-	    name = str.strip(tr.text_content())
-	    if addressCheck1 in name or addressCheck2 in name or addressCheck3 in name or addressCheck4 in name or addressCheck5 in name or addressCheck6 in name:
-	       # print name
-	        nameList.append(name)
+		#Targeting the names and location
+		#Name of the location
+		for tr in root.cssselect("div#Rentals tr td span a"):
+		    print "Scraping Names Page:" + str(page) + "..."
+		    name = str.strip(tr.text_content())
+		    if addressCheck1 in name or addressCheck2 in name or addressCheck3 in name or addressCheck4 in name or addressCheck5 in name or addressCheck6 in name:
+		       # print name
+		        nameList.append(name)
+		    
+		#Price Listing
+		for tr in root.cssselect(".t-last"):
+		    print "Scraping Prices Page:" + str(page) + "..."
+		    price = str.strip(tr.text_content())
+		    price = ''.join(price.split())
+		   # print price
+		    priceList.append(price)
+
+		page+=1
+
+	i = 0
+	while (i!=len(nameList) and i!=len(priceList)):
+	    master[nameList[i]] = priceList[i]
+	    i+=1
 	    
-	#Price Listing
-	for tr in root.cssselect(".t-last"):
-	    print "Scraping Prices Page:" + str(page) + "..."
-	    price = str.strip(tr.text_content())
-	    price = ''.join(price.split())
-	   # print price
-	    priceList.append(price)
 
-	page+=1
+    #for location,price in master.items():
+		#print "Name: " + location +"  ---  " + "Price: " + price
 
-i = 0
-while (i!=len(nameList) and i!=len(priceList)):
-    master[nameList[i]] = priceList[i]
-    i+=1
+	return master
+	
+def sort_prices(master):
+    sorted_master = master
+    for key, value in master.iteritems():
+		price = master[key]
+		if "-" in price:
+		    price = price[1:price.find("-")]
+		else:
+		    price = price[1:]
+		print "New Price: " + price
+		sorted_master [key] = int(price)
+    return sorted(sorted_master)
     
+    
+master = scrape()
 
-for location,price in master.items():
-	print "Name: " + location +"  ---  " + "Price: " + price
+sorted_master = sort_prices(master)
+print sorted_master
