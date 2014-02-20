6b690658f31f76c8c9bc38de8980e752e4bfb6ce
Wasiur Rahman <wrahman0@gmail.com>
2014-02-20 01:06:31 -0500

    Integration of all the parts

--- scrape.js
+++ scrape.js
@@ -1,7 +1,6 @@
 var request = require ('request'),
 	cheerio = require ('cheerio');
 
-
 var addressCheck1 = "Waterloo, ON";
 var addressCheck2 = "waterloo, ON";
 var addressCheck3 = "Cambridge, ON";
@@ -30,14 +29,25 @@ while (pages<maxNumberPages)
 					locations.push(this.text().trim());
 				}
 
-			});
-
-			console.log(locations);
-			console.log(locations.length);
-
+			});			
 		}
-		
 	});
+
+	console.log(locations);
+	console.log(locations.length);
+
 	pages++;
 }
 
+
+var map;
+function initialize() {
+  var mapOptions = {
+    zoom: 8,
+    center: new google.maps.LatLng(43.472285, -80.544858);
+  };
+  map = new google.maps.Map(document.getElementById('map-canvas'),
+      mapOptions);
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
